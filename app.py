836b0301332d3cdcf97346c4b7b09e0a17951f3e
floriancaro <floriancaro0510@gmail.com>
2021-05-30 20:18:02 +0200

    Fix map tooltips
    Tooltips now show the number of hired foreigners whereas column height 
    is computed from logged number of foreigners.

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
-import plotly.express as px
+# import plotly.express as px
 from io import StringIO
+import geopandas
 
 # create dateparser to be safe
 from datetime import datetime
@@ -38,8 +39,15 @@ def load_data(nrows = 50000):
 
     return data
 
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+
+# Load data
 data = load_data()
 edited_data = data.copy()
+
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Loading data ... Done!") # (using st.cache)
 
 # drop NAs
 edited_data.dropna(subset=['latitude', 'longitude'], inplace = True)
@@ -51,38 +59,77 @@ edited_data['employment_start_date_converted'] = edited_data['employment_start_d
 midpoint = (np.average(edited_data['latitude']), np.average(edited_data['longitude']))
 
 # create dataframe with logged frequency of entries for each city
-logged_data = edited_data[['latitude', 'longitude']].drop_duplicates()
 frequency = (edited_data[['latitude', 'longitude']].groupby(edited_data[['latitude', 'longitude']].columns.tolist()).size().reset_index().rename(columns={0:'records'})) # compute frequency of each location in the data
-for index, row in frequency.iterrows():
-    logged_data = frequency.loc[frequency.index.repeat(np.log(frequency.records + 1)**2)].reset_index(drop=True)
 
-logged_data = logged_data.reset_index()
+logged_data = frequency
+logged_data['log_frequency'] = 0
+for index, row in frequency.iterrows():
+    logged_data.loc[index,'log_frequency'] = np.log(row.records + 1)**2
+
+max_records = np.max(logged_data.log_frequency)
+logged_data['log_frequency_max_percentage'] = logged_data['log_frequency']/max_records
+
+logged_data = logged_data.reset_index(drop=True)
 st.write(logged_data)
+# gdf = geopandas.GeoDataFrame(logged_data, geometry=geopandas.points_from_xy(logged_data.longitude, logged_data.latitude))
+# gdf.to_file("output.geo.json", driver='GeoJSON')
+
+# implement drop down menu to filter for specific nationalities
+# st.map(logged_data)
+
+# viewstate range - yet to implement
+LONGITUDE_RANGE = [midpoint[1]-10, midpoint[1]+10]
+LATITUDE_RANGE = [midpoint[0]-10, midpoint[0]+10]
+
+column_layer = pdk.Layer(
+    "ColumnLayer",
+    data = logged_data,
+    get_position=["longitude", "latitude"],
+    get_elevation="log_frequency",
+    elevation_scale=3000,
+    elevation_range=[0,7500],
+    radius=2000,
+    get_fill_color=["log_frequency_max_percentage * 250 + 120", 100, 100, 220],
+    pickable=True,
+    auto_highlight=True,
+)
+
+# DATA_PATH = os.path.join(os.getcwd(),"output.geo.json")
+# st.write(gdf)
+#
+# geojson = pdk.Layer(
+#     "GeoJsonLayer",
+#     data = gdf,
+#     opacity=0.8,
+#     stroked=False,
+#     filled=True,
+#     extruded=True,
+#     wireframe=True,
+#     pickable=True,
+#     auto_highlight=True,
+#     get_elevation="log_frequency",
+#     get_fill_color="[255, 255, 140]",
+#     get_line_color=[255, 255, 255],
+# )
+
+# define view state
+view_state = pdk.ViewState(
+    latitude= midpoint[0],
+    longitude= midpoint[1],
+    zoom= 6,
+    pitch= 45,
+    min_zoom=4.5,
+    max_zoom=7,
+)
 
 st.write(pdk.Deck(
     map_style="mapbox://styles/mapbox/light-v9",
-    initial_view_state={
-        "latitude": midpoint[0],
-        "longitude": midpoint[1],
-        "zoom": 7,
-        "pitch": 50,
-    },
+    initial_view_state=view_state,
     layers=[
-        pdk.Layer(
-            "HexagonLayer",
-            data = logged_data,
-            get_position=['longitude','latitude'],
-            records=['records'],
-            auto_highlight=True,
-            radius=2500,
-            extruded=True,
-            pickable=True,
-            elevation_scale=350,
-            elevation_range=[0,500],
-        ),
+        column_layer,
     ],
     tooltip={
-        "text": "{elevationValue}",
+        "text": "{records}",
         "style": {
             "backgroundColor": "steelblue",
             "color": "white"
@@ -110,14 +157,14 @@ st.bar_chart(hist_values)
 # compute corresponding average and variance of wages
 average_employment_duration = np.average(edited_data['time_employed_converted'])
 variance_employment_duration = np.var(edited_data['time_employed_converted'])
-st.markdown("Average employment duration: %" % (average_employment_duration))
-st.markdown("Variance: %" % (variance_employment_duration))
+st.markdown("Average employment duration: {}".format(average_employment_duration))
+st.markdown("Standard error: {}".format(np.sqrt(variance_employment_duration)))
 
 
 
 # Create histogram showing the distribution of wages among hired foreigners
 st.subheader("Distribution of Wages among Hired Foreigners")
-hist_values = np.histogram(edited_data['wage_converted_into_yen'], bins = 20, range = (0,2000))
+hist_values = np.histogram(edited_data['wage_converted_into_yen'], bins = 10, range = (0,2000))
 hist_values = pd.DataFrame(hist_values).T
 hist_values = hist_values.rename(columns={0:'wage', 1:'index'}).set_index('index')
 st.bar_chart(hist_values)
@@ -125,8 +172,8 @@ st.bar_chart(hist_values)
 # compute corresponding average and variance of wages
 average_wage = np.average(edited_data['wage_converted_into_yen'])
 variance_wage = np.var(edited_data['wage_converted_into_yen'])
-st.markdown("Average Wage: %" % (average_wage))
-st.markdown("Variance: %" % (variance_wage))
+st.markdown("Average Wage: {}".format(average_wage))
+st.markdown("Standard error: {}".format(np.sqrt(variance_wage)))
 
 
 # add a checkbox in order to not always show the raw data
